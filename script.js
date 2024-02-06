cfe4a24a055f72026b85a3975f347d6d4eadeeda
ale-morillas <alecorm.edx@gmail.com>
2024-02-06 09:33:10 -0500

    Add operate function that return a mathematical operation depending on the operator

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function multiplyFunction(a, b) {
 function divideFunction(a, b) {
   return a / b;
 }
+
+// Variables
+let num1;
+let operator;
+let num2;
+
+// Function Operate
+function operate(num1, operator, num2) {
+  if (operator === "+") {
+    return addFunction(num1, num2);
+  } else if (operator === "-") {
+    return subtractFunction(num1, num2);
+  } else if (operator === "*") {
+    return multiplyFunction(num1, num2);
+  } else if (operator === "/") {
+    return divideFunction(num1, num2);
+  }
+}

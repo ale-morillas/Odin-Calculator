6b97e96e546790c5c71f03f86186062632032398
ale-morillas <alecorm.edx@gmail.com>
2024-02-06 09:22:00 -0500

    Add basic maths operation functions.
    Add addFunction, subtractFunction, multiplyFunction and divideFunction
    to perform sum, rest, multiplication and division operations.

---
+++ script.js
@@ -0,0 +1,17 @@
+// Basic Math Operation Functions
+
+function addFunction(a, b) {
+  return a + b;
+}
+
+function subtractFunction(a, b) {
+  return a - b;
+}
+
+function multiplyFunction(a, b) {
+  return a * b;
+}
+
+function divideFunction(a, b) {
+  return a / b;
+}
